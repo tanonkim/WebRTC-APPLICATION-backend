c5249d8354c030a719b8e756609494223cc48c06
tanonkim <robbins4bos@gmail.com>
2022-03-28 12:00:09 +0900

    fix : typo, max_length

--- users/models.py
+++ users/models.py
@@ -16,12 +16,12 @@ class UserManager(BaseUserManager):
         user.set_password(password)        
         return user     
 
-class user(AbstractBaseUser):
+class User(AbstractBaseUser):
     objects = UserManager()
 
-    email      = models.EmailField(max_length=255, unique=True)
-    first_name = models.CharField(max_length=50)
-    last_name  = models.CharField(max_length=50)
+    email      = models.EmailField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=25)
+    last_name  = models.CharField(max_length=25)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)  
 
